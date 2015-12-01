07ced3322a749bf6d8a4b830aacb393ed167a570
Hammad Mazhar <hammad.maz@gmail.com>
2015-12-01 12:36:29 -0600

    Add some math constants to defines

--- defines.h
+++ defines.h
@@ -13,3 +13,15 @@
 #define CUDA_SHARED
 #define CUDA_GLOBAL
 #endif
+
+
+#define C_Pi 4 * atan(1.0f)
+#define C_2Pi 2.0f * C_Pi
+#define C_InvPi 1.0f / C_Pi
+#define C_Inv2Pi 0.5f / C_Pi
+
+#define C_DegToRad C_Pi / 180.0f
+#define C_RadToDeg 180.0f / C_Pi
+
+#define C_EPSILON FLT_EPSILON
+#define C_LARGE_REAL FLT_MAX
